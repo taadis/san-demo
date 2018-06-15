3e053eaced9474064d276b8b77786bff397bd589
taadis <i@taadis.com>
2018-06-15 14:15:12 +0800

    update Components

--- Components/JsRunComponent.js
+++ Components/JsRunComponent.js
@@ -2,8 +2,8 @@
 * JS 运行组件
 */ 
 var JsRunComponent = san.defineComponent({
-    template: `<div>
-        <textarea id="jsCode" style="width:319px;height:127px;">
+    template: `<div>        
+        <textarea id="jsCode" class="layui-code" lay-title="javascript" lay-skin="notepad" style="margin-top: 50px;width:319px;height:127px;">
         layer.open({
             title: '在线调试',
             content: '可以填写任意的layer代码'
